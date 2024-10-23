5425b229e4af17d30e4b562407c5a598d304eacc
datungLA <97591324+datungLA@users.noreply.github.com>
2024-10-22 19:30:26 -0700

    Exercise 3 Optional Challenge

--- src/components/Expense/ExpenseItem.tsx
+++ src/components/Expense/ExpenseItem.tsx
@@ -1,25 +1,45 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Expense } from "../../types/types";
 import { AppContext } from "../../context/AppContext";
 
 const ExpenseItem = (currentExpense: Expense) => {
   // Exercise: Consume the AppContext here
   const { expenses, setExpenses } = useContext(AppContext)
-
+  const [editField, setEditField] = useState<string | null>(null);
+  const [editExpense, setEditExpense] = useState<Expense>(currentExpense);
   const handleDeleteExpense = (currentExpense: Expense) => {
     // Exercise: Remove expense from expenses context array
     const updatedExpense = expenses.filter((expense) => expense.id !== currentExpense.id)
     setExpenses(updatedExpense);
   };
-
+  const handleEditExpense = () => {
+    const updatedExpense = expenses.map((expense) => expense.id === currentExpense.id ? { ...expense, name: editExpense.name, cost: editExpense.cost } : expense);
+    setExpenses(updatedExpense);
+    setEditField(null);
+  };
+  const handleBlur = () => {
+    handleEditExpense();
+  }
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
-      <div>{currentExpense.name}</div>
-      <div>${currentExpense.cost}</div>
       <div>
-        <button onClick={() => handleDeleteExpense(currentExpense)}>x</button>
+        {editField === "name" ? (
+          <input type="text" value={editExpense.name} onChange={(e) => setEditExpense({ ...editExpense, name: e.target.value })} onBlur={handleBlur} autoFocus />
+        ) : (
+          <div onClick={() => setEditField("name")}>{currentExpense.name}</div>
+        )
+        }
       </div>
-    </li>
+      <div>
+        {editField === "cost" ? (
+          <input type="text" value={editExpense.cost} onChange={(e) => setEditExpense({ ...editExpense, cost: Number(e.target.value) })} onBlur={handleBlur} autoFocus />
+        ) : (
+          <div onClick={() => setEditField("cost")}>${currentExpense.cost}</div>
+        )
+        }
+      </div>
+      <button onClick={() => handleDeleteExpense(currentExpense)}>x</button>
+    </li >
   );
 };
 
