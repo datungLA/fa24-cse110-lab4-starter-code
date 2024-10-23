84ec56034798d58111dbd084fa034e128bbb7b1e
datungLA <97591324+datungLA@users.noreply.github.com>
2024-10-22 20:26:32 -0700

    Exercise 3 Optional Challenge: Edit budget

--- src/components/Budget/Budget.tsx
+++ src/components/Budget/Budget.tsx
@@ -1,11 +1,19 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../context/AppContext";
 
 const Budget = () => {
-  const { budget } = useContext(AppContext);
+  const { budget, setBudget } = useContext(AppContext);
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editBudget, setEditBudget] = useState<number>(budget)
+  const handleBlur = () => {
+    setBudget(editBudget)
+    setEdit(false)
+  }
   return (
     <div className="alert alert-secondary p-3 d-flex align-items-center justify-content-between">
-      <div>Budget: ${budget}</div>
+      {edit ? (<input type="text" value={editBudget} onChange={(e) => setEditBudget(Number(e.target.value))} onBlur={handleBlur} autoFocus />) : (
+        <div onClick={() => setEdit(true)}>Budget: ${budget}</div>
+      )}
     </div>
   );
 };
